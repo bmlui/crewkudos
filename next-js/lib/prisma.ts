d2298f475fc0cf2b9f4fcc9d067201b255f2b18b
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2025-07-01 12:14:41 -0400

    prisma fix

--- next-js/lib/prisma.ts
+++ next-js/lib/prisma.ts
@@ -1,5 +1,5 @@
 // lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../prisma/app/generated/prisma/client';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
